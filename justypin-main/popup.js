2bde7f87d58677f957ce6e2c7eef6f56f82f0164
A-Little-Mouse <thunderboiigod@gmail.com>
2025-06-03 19:24:31 +0545

    Update popup.js
    changed some names

--- justypin-main/popup.js
+++ justypin-main/popup.js
@@ -6,19 +6,19 @@ function popupopen(){
 
 /* scroll */
 var targetDivision = document.getElementById('window');/* place where you type */
-var sussyniqqa= 1
+var cursorpoint= 1
 var scrollcount=0;
 function scroll(charIndex){
 var offp= document.querySelectorAll('.window span')[sussyniqqa].offsetTop;
 var offw= document.querySelectorAll('.window span')[charIndex].offsetTop;
         if(offw>offp){
             scrollcount+=1;
-            sussyniqqa=charIndex;
+            cursorpoint=charIndex;
             let sundari=(offw-offp)*scrollcount
             targetDivision.scrollTop = sundari ;
         }
         if(offw<offp){
-            sussyniqqa=charIndex-1;
+            cursorpoint=charIndex-1;
             scrollcount-=1;
             let sundar=(offp-offw)*scrollcount;
             targetDivision.scrollTop = sundar;
